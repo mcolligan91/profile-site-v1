ab33ccd751495faf8d1ff244372fc1fa37ec3178
mcolligan91 <60613289+mcolligan91@users.noreply.github.com>
2024-05-28 18:25:46 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/profile-site/precache-manifest.cddae2def536ff6b1ea548e46a3005bc.js"
+  "/profile-site-v1/precache-manifest.3d0d8c1066517058ca8101fc3ad3d42a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/profile-site/index.html", {
+workbox.routing.registerNavigationRoute("/profile-site-v1/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
